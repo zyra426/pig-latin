3c88fda5b52e0222f2c74f76ff0afc1671b6bc1d
Zach Schroder <zachary@limabean.co.za>
2018-04-30 09:37:12 +0200

    added variables for arrays

--- piglatin/views.py
+++ piglatin/views.py
@@ -8,13 +8,15 @@ def translate(request):
     text = request.GET['origText'].lower()
 
     translation = ''
+    vowels = ["a", "e", "i", "o", "u"]
+    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
     for word in text.split():
-        if word[0] in ["a", "e", "i", "o", "u"]:
+        if word[0] in vowels:
             # vowel
             translation += word
             translation += "way "
-        elif word[1] in ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]: 
+        elif word[1] in consonants: 
             # consonant
             translation += word[2:]
             translation += word[0:2] + "ay "
